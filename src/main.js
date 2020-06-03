23701f7a41302aa60a6aa2d27e3a0edd8194235d
Slava <slava@example.com>
2020-06-03 13:15:24 +0300

    clear

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import Axios from 'axios';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = Axios;
 
 new Vue({
   router,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Allfilms from '../components/Allfilms.vue'
 import Detailsfilm from '../components/Detailsfilm.vue'
-import Axios from 'axios'
+
 
 Vue.use(VueRouter);
-Vue.prototype.$http = Axios;
+
 
   const routes = [
   {
@@ -17,14 +17,6 @@ Vue.prototype.$http = Axios;
     path: '/detailsfilm',
     name: 'Detailsfilm',
     component: Detailsfilm
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ];
 
